7b7e8e27b931c3205b6d16ac587b05663aacd4bb
Nils Kurzawa <nilskurzawa@yahoo.de>
2013-08-01 16:25:22 +0200

    added php-config-datei

---
+++ php_config.php
@@ -0,0 +1,10 @@
+<?php 
+
+$host = "localhost"; // Host name 
+$username = "root"; // Mysql username 
+$password = ""; // Mysql password 
+$db_name = "nrps_designer"; // Database name 
+$substrates = "substrates"; // Table name 
+//echo $username;
+
+?>
