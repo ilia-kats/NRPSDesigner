b20180a2b2c09e3071365d630de3c0a27bb974df
nikos <nikos.ignatiadis01@gmail.com>
2013-08-13 23:28:01 +0200

    select2 working, homepage with nice redirect

--- Django/DjangoNrps/DjangoNrps/urls.py
+++ Django/DjangoNrps/DjangoNrps/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 from databaseInput.views import PfamView, UserDetailView, HomeTemplateView, ProfileTemplateView
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
