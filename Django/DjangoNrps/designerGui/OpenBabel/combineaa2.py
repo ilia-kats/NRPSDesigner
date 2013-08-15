979e965463b1f4fa6668cd3ae7405bcfe6feda30
Ilia Kats <ilia-kats@gmx.net>
2013-08-15 16:26:45 +0200

    colore amino acid residues differently

--- Django/DjangoNrps/designerGui/OpenBabel/combineaa2.py
+++ Django/DjangoNrps/designerGui/OpenBabel/combineaa2.py
@@ -4,41 +4,66 @@
 import sys
 import openbabel as ob
 
+def makeResidue(mol, idx, aaatoms):
+    res = mol.NewResidue()
+    res.SetNum(idx)
+    for atom in ob.OBMolAtomIter(mol):
+        if atom.GetIdx() not in aaatoms:
+            res.AddAtom(atom)
+
 mol = ob.OBMol()
 conv = ob.OBConversion()
 pattern = ob.OBSmartsPattern()
 pattern.Init("[NX3][$([CX4H1]([*])),$([CX4H2])][CX3](=[OX1])[OX2]")
 builder = ob.OBBuilder()
 conv.SetInAndOutFormats("sdf", "svg")
+conv.AddOption("d", ob.OBConversion.OUTOPTIONS)
 conv.ReadFile(mol, sys.argv[1])
-pattern.Match(mol)
-mollist = pattern.GetUMapList()
-natom = mol.GetAtom(mollist[0][0])
 
+pattern.Match(mol)
+mollist = pattern.GetUMapList()[0]
+natom = mol.GetAtom(mollist[0])
+makeResidue(mol, 0, mollist)
+
+i = 1
 for aa in sys.argv[2:]:
     mol2 = ob.OBMol()
-    conv.ReadString(mol2, aa)
+    conv.ReadFile(mol2, aa)
     pattern.Match(mol2)
-    mollist = pattern.GetUMapList()
+    mollist = pattern.GetUMapList()[0]
+    makeResidue(mol2, i, mollist)
 
-    catom = mol2.GetAtom(mollist[0][2])
-    oatom = mol2.GetAtom(mollist[0][4])
+    catom = mol2.GetAtom(mollist[2])
+    oatom = mol2.GetAtom(mollist[4])
 
     molnatoms = mol.NumAtoms()
     mol += mol2
 
     builder.Connect(mol, natom.GetIdx(), molnatoms + catom.GetIdx())
     foatom = mol.GetAtom(molnatoms + oatom.GetIdx())
-    fnatom = mol.GetAtom(molnatoms + mol2.GetAtom(mollist[0][0]).GetIdx())
+    fnatom = mol.GetAtom(molnatoms + mol2.GetAtom(mollist[0]).GetIdx())
     mol.DeleteHydrogens(foatom)
     mol.DeleteAtom(foatom)
-    #mol.DeleteHydrogens()
-    #mol.AddHydrogens()
 
     natom.SetImplicitValence(3)
     mol.DeleteHydrogens(natom)
     mol.AddHydrogens(natom)
     natom = fnatom
+    i += 1
 
 builder.Build(mol)
-return conv.WriteFile(mol)
+for res in ob.OBResidueIter(mol):
+    if res.GetNum() % 2 > 0:
+        color = "red"
+    else:
+        color = "green"
+    for atom in ob.OBResidueAtomIter(res):
+        for bond in ob.OBAtomBondIter(atom):
+            data = ob.OBPairData()
+            data.SetAttribute("color")
+            data.SetValue(color)
+            bond.CloneData(data)
+mol.DeleteHydrogens()
+#mol.AddHydrogens()
+
+conv.WriteFile(mol, "test.svg")
--- Django/DjangoNrps/designerGui/views.py
+++ Django/DjangoNrps/designerGui/views.py
@@ -17,7 +17,15 @@ class SpeciesListView(ListView):
         return context
   
 def make_structure(request):
+    def makeResidue(mol, idx, aaatoms):
+        res = mol.NewResidue()
+        res.SetNum(idx)
+        for atom in ob.OBMolAtomIter(mol):
+            if atom.GetIdx() not in aaatoms:
+                res.AddAtom(atom)
+
     aminoacids = request.GET.getlist("as")
+    color = request.GET.get("rescol", "#3EC1CD")
 
     mol = ob.OBMol()
     conv = ob.OBConversion()
@@ -25,37 +33,48 @@ def make_structure(request):
     pattern.Init("[NX3][$([CX4H1]([*])),$([CX4H2])][CX3](=[OX1])[OX2]")
     builder = ob.OBBuilder()
     conv.SetInAndOutFormats("sdf", "svg")
+    conv.AddOption("d", ob.OBConversion.OUTOPTIONS)
     conv.ReadString(mol, str(Substrate.objects.get(pk=int(aminoacids[0])).structure))
     pattern.Match(mol)
-    mollist = pattern.GetUMapList()
-    natom = mol.GetAtom(mollist[0][0])
+    mollist = pattern.GetUMapList()[0]
+    natom = mol.GetAtom(mollist[0])
+    makeResidue(mol, 0, mollist)
 
+    i = 1
     for aa in aminoacids[1:]:
         mol2 = ob.OBMol()
         conv.ReadString(mol2,str(Substrate.objects.get(pk=int(aa)).structure))
         pattern.Match(mol2)
-        mollist = pattern.GetUMapList()
+        mollist = pattern.GetUMapList()[0]
+        makeResidue(mol2, i, mollist)
 
-        catom = mol2.GetAtom(mollist[0][2])
-        oatom = mol2.GetAtom(mollist[0][4])
+        catom = mol2.GetAtom(mollist[2])
+        oatom = mol2.GetAtom(mollist[4])
 
         molnatoms = mol.NumAtoms()
         mol += mol2
 
         builder.Connect(mol, natom.GetIdx(), molnatoms + catom.GetIdx())
         foatom = mol.GetAtom(molnatoms + oatom.GetIdx())
-        fnatom = mol.GetAtom(molnatoms + mol2.GetAtom(mollist[0][0]).GetIdx())
+        fnatom = mol.GetAtom(molnatoms + mol2.GetAtom(mollist[0]).GetIdx())
         mol.DeleteHydrogens(foatom)
         mol.DeleteAtom(foatom)
-        #mol.DeleteHydrogens()
-        #mol.AddHydrogens()
 
         natom.SetImplicitValence(3)
         mol.DeleteHydrogens(natom)
         mol.AddHydrogens(natom)
         natom = fnatom
+        i += 1
 
     builder.Build(mol)
+    for res in ob.OBResidueIter(mol):
+        for atom in ob.OBResidueAtomIter(res):
+            for bond in ob.OBAtomBondIter(atom):
+                data = ob.OBPairData()
+                data.SetAttribute("color")
+                data.SetValue(color)
+                bond.CloneData(data)
+    mol.DeleteHydrogens()
     svg = conv.WriteString(mol)
     return HttpResponse(svg, mimetype="image/svg+xml")
 
