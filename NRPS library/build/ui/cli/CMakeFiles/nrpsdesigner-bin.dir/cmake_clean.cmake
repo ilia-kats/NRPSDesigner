ce1db193b2720728f67da446139a10bbaf60b2d3
Hetitus <tim.heineman@gmail.com>
2013-08-27 16:40:30 +0200

    sample modal window

---
+++ NRPS library/build/ui/cli/CMakeFiles/nrpsdesigner-bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/nrpsdesigner-bin.dir/main.cpp.o"
+  "nrpsdesigner.pdb"
+  "nrpsdesigner"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/nrpsdesigner-bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
