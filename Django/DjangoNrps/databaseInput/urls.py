0b90e9da1b355b2cfa061a0bb12ffc352b820662
Konrad Herbst <k.herbst@stud.uni-heidelberg.de>
2013-09-21 02:01:51 +0200

    fixed some jquery bugs; removed dependencies on 'requests' and discarded sauceFunc

--- Django/DjangoNrps/databaseInput/urls.py
+++ Django/DjangoNrps/databaseInput/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
-from databaseInput.views import sauceFunc, cdsInput, domainInput, origin_add, origin_ajax_save, msa_domain_view
-
+#from databaseInput.views import sauceFunc, cdsInput, domainInput, origin_add, origin_ajax_save, msa_domain_view
+from databaseInput.views import cdsInput, domainInput, origin_add, origin_ajax_save, msa_domain_view
 
 urlpatterns = patterns('',
-    url(r'^submit/', sauceFunc, name="sauceFunc"),
+    #url(r'^submit/', sauceFunc, name="sauceFunc"),
     url(r'^$', cdsInput, name="pfam"),
     url(r'^addOrigin', origin_add, name="addOrigin"),
     url(r'^getDomainForm/', domainInput, name="getDomainForm"),
--- Django/DjangoNrps/databaseInput/views.py
+++ Django/DjangoNrps/databaseInput/views.py
@@ -13,7 +13,7 @@ from django.forms.models import inlineformset_factory
 from django.contrib.auth.decorators import login_required
 
 import json
-import requests
+#import requests
 import time
 
 from xml.dom.minidom import parseString
@@ -172,44 +172,43 @@ class UserDetailView(DetailView):
     template_name = 'databaseInput/user_detail.html'
     slug_field = "username"
 
-def sauceFunc(request):
-    #first read FASTA file and translate sequence to protein!
-    #dnaSeq  = SeqIO.read("bpsa.fasta", "fasta",IUPAC.unambiguous_dna).seq
-    sequence = request.POST["sequence"].replace("\n","").replace("\t","")
-    dnaSeq = Seq(sequence, IUPAC.unambiguous_dna)
-    protSeq = dnaSeq.translate(to_stop=True)
+#def sauceFunc(request):
+    ##first read FASTA file and translate sequence to protein!
+    ##dnaSeq  = SeqIO.read("bpsa.fasta", "fasta",IUPAC.unambiguous_dna).seq
+    #sequence = request.POST["sequence"].replace("\n","").replace("\t","")
+    #dnaSeq = Seq(sequence, IUPAC.unambiguous_dna)
+    #protSeq = dnaSeq.translate(to_stop=True)
 
-    #send PFAM request 1
-    pfamUrl = "http://pfam.sanger.ac.uk/search/sequence"
-    pfamParams = {'seq':str(protSeq), 'output':'xml'}
-    pfamRequest= requests.get(pfamUrl, params=pfamParams)
+    ##send PFAM request 1
+    #pfamUrl = "http://pfam.sanger.ac.uk/search/sequence"
+    #pfamParams = {'seq':str(protSeq), 'output':'xml'}
+    #pfamRequest= requests.get(pfamUrl, params=pfamParams)
 
-    #extract result link from XML by converting to DOM, extracting result url tag, removing tag elements from string
-    pfamDom = parseString(pfamRequest.text)
-    pfamJobId = pfamDom.getElementsByTagName('job')[0].attributes["job_id"].value
-    pfamResultUrl = "http://pfam.sanger.ac.uk/search/sequence/resultset/" + pfamJobId
-    pfamGraphicUrl = "http://pfam.sanger.ac.uk/search/sequence/graphic/" + pfamJobId
+    ##extract result link from XML by converting to DOM, extracting result url tag, removing tag elements from string
+    #pfamDom = parseString(pfamRequest.text)
+    #pfamJobId = pfamDom.getElementsByTagName('job')[0].attributes["job_id"].value
+    #pfamResultUrl = "http://pfam.sanger.ac.uk/search/sequence/resultset/" + pfamJobId
+    #pfamGraphicUrl = "http://pfam.sanger.ac.uk/search/sequence/graphic/" + pfamJobId
 
-    #wait a bit
-    time.sleep(1)
+    ##wait a bit
+    #time.sleep(1)
 
-    # keep sending PFAM request 2 until it works
-    while True:
-        pfamResultRequest = requests.get(pfamResultUrl)
-        if pfamResultRequest.status_code == 200:
-            break
-        elif pfamResultRequest.status_code == 202:
-            time.sleep(1)
-        else:
-            break #THIS SHOULD actually throw exception!!!
+    ## keep sending PFAM request 2 until it works
+    #while True:
+        #pfamResultRequest = requests.get(pfamResultUrl)
+        #if pfamResultRequest.status_code == 200:
+            #break
+        #elif pfamResultRequest.status_code == 202:
+            #time.sleep(1)
+        #else:
+            #break #THIS SHOULD actually throw exception!!!
 
-    # read xml DOM, find stuff corresponding to domains
-    #pfamResultDom = parseString(pfamResultRequest.text)
-    #pfamResultMatches = pfamResultDom.getElementsByTagName('match')
-    #pfamDomains = []
-    #for match in pfamResultMatches:
-    #   pfamDomains.append(match.attributes["id"].value)
-    pfamResultRequest = requests.get(pfamGraphicUrl)
-    import pdb; pdb.set_trace()
-    return HttpResponse(pfamResultRequest.text[1:-1], mimetype="text/plain")
-
+    ## read xml DOM, find stuff corresponding to domains
+    ##pfamResultDom = parseString(pfamResultRequest.text)
+    ##pfamResultMatches = pfamResultDom.getElementsByTagName('match')
+    ##pfamDomains = []
+    ##for match in pfamResultMatches:
+    ##   pfamDomains.append(match.attributes["id"].value)
+    #pfamResultRequest = requests.get(pfamGraphicUrl)
+    #import pdb; pdb.set_trace()
+    #return HttpResponse(pfamResultRequest.text[1:-1], mimetype="text/plain")
