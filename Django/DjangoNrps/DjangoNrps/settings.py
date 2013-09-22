de901ea6f60f065f7e4649cf8481f9e221a5b9eb
Ilia Kats <ilia-kats@gmx.net>
2013-09-22 15:36:34 +0200

    fix user profile URLs, use the default registration backend with confirmation email, auto-login user after activation

--- Django/DjangoNrps/DjangoNrps/settings.py
+++ Django/DjangoNrps/DjangoNrps/settings.py
@@ -123,3 +123,19 @@ STATICFILES_FINDERS = (
 )
 
 LOGIN_REDIRECT_URL = '/user/profile'
+ACCOUNT_ACTIVATION_DAYS = 7
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Host for sending e-mail.
+EMAIL_HOST = 'localhost'
+
+# Port for sending e-mail.
+EMAIL_PORT = 25
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+
+DEFAULT_FROM_EMAIL='kats@stud.uni-heidelberg.de'
--- Django/DjangoNrps/DjangoNrps/urls.py
+++ Django/DjangoNrps/DjangoNrps/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from databaseInput.views import  UserDetailView, HomeTemplateView, ProfileTemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import login
+from registration import signals
 
 admin.autodiscover()
 
@@ -9,11 +11,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DjangoNrps.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$', HomeTemplateView.as_view()),
+	url(r'^$', HomeTemplateView.as_view(), name='home'),
 	url(r'^admin/', include(admin.site.urls)),
-    url(r'^user/profile', ProfileTemplateView.as_view()),
-    url(r'^user/', include('registration.backends.simple.urls')),
-    url(r'^user/(?P<slug>\w+)/$', UserDetailView.as_view(), name="profile"),
+    url(r'^user/profile', ProfileTemplateView.as_view(), name="userprofile"),
+    url(r'^user/', include('registration.backends.default.urls')),
+    url(r'^users/(?P<slug>\w+)/$', UserDetailView.as_view(), name="profile"),
     url(r'^tool/', include('designerGui.urls')),
     url(r'^gibthon/', include('gibson.urls')),
     url(r'^fragment/', include('fragment.urls')),
@@ -21,3 +23,9 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns()
+
+def login_on_activation(user, request, **kwargs):
+    user.backend='django.contrib.auth.backends.ModelBackend'
+    login(request, user)
+
+signals.user_activated.connect(login_on_activation)
