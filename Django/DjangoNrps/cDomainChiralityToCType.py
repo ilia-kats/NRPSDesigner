6f7e168509fda5a3b03f812177a967043d996eb9
Ilia Kats <ilia-kats@gmx.net>
2013-10-03 10:36:27 +0200

    remove all the stuff not required for submission. closes #22

--- Django/DjangoNrps/cDomainChiralityToCType.py
+++
@@ -1,8 +0,0 @@
-from databaseInput.models import Domain, Type
-
-cDomains = Domain.objects.filter(domainType__name = 'C')
-domTypes = {'L': Type.objects.get(name = 'C_L'),
-	'D': Type.objects.get(name = 'C_D')}
-for domain in cDomains:
-	domain.domainType = domTypes[domain.chirality]
-	domain.save()
